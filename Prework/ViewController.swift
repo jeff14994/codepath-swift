e5b4412b16c2a30c612f043bcd299d37618e9b7e
jeff14994 <jeff14994@gmail.com>
2021-12-24 16:31:53 -0700

    Complete Updating from Setting Page to View Page

--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -13,11 +13,20 @@ class ViewController: UIViewController {
     @IBOutlet weak var tipAmountLabel: UILabel!
     @IBOutlet weak var tipControl: UISegmentedControl!
     @IBOutlet weak var totalLabel: UILabel!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         self.title = "Tip Calculator"
+        // Access UserDefaults
+        let defaults = UserDefaults.standard
+        let tip1 = defaults.double(forKey: "tip1")
+        let tip2 = defaults.double(forKey: "tip2")
+        let tip3 = defaults.double(forKey: "tip3")
+        // Change Tip Control with user's inputs
+        tipControl.setTitle(String(tip1*100) + "%", forSegmentAt: 0)
+        tipControl.setTitle(String(tip2*100) + "%", forSegmentAt: 1)
+        tipControl.setTitle(String(tip3*100) + "%", forSegmentAt: 2)
     }
 
     @IBAction func calculateTip(_ sender: Any) {
@@ -31,6 +40,10 @@ class ViewController: UIViewController {
         print("tip1: ", tip1)
         print("tip2: ", tip2)
         print("tip3: ", tip3)
+        // Change Tip Control with user's inputs
+        tipControl.setTitle(String(tip1*100) + "%", forSegmentAt: 0)
+        tipControl.setTitle(String(tip2*100) + "%", forSegmentAt: 1)
+        tipControl.setTitle(String(tip3*100) + "%", forSegmentAt: 2)
         // Get Total tip by multiplying tip * tipPercentage
         let tipPercentages = [tip1, tip2, tip3]
         let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
